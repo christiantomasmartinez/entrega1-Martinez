ce4e4868b54830afc99c0d8ee52cb6b0bb771fb1
christiantomasmartinez <christiantomas@hotmail.com>
2023-04-25 16:05:12 -0300

    Probando avatar usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 
 # Create your models here.
 
 class InfoExtra(models.Model):
     avatar = models.ImageField(upload_to='avatares', null=True, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+# class InfoExtra(models.Model):
+#     avatar = models.ImageField(upload_to='avatares/', blank=True, null=True)
